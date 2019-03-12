b08ea68fd870a1f9d92fefae23bbb91ebb342ab1
Gary Tokman <hello@garytokman.me>
2019-03-11 21:53:47 -0400

    build out login screen

---
+++ LoginExample/LoginExample/DesignableView.swift
@@ -0,0 +1,37 @@
+//
+//  DesignableView.swift
+//  LoginExample
+//
+//  Created by Gary Tokman on 3/10/19.
+//  Copyright © 2019 Gary Tokman. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class DesignableView: UIView {
+    @IBInspectable var shadowColor: UIColor = UIColor.clear {
+        didSet {
+            layer.shadowColor = shadowColor.cgColor
+        }
+    }
+
+    @IBInspectable var shadowRadius: CGFloat = 0 {
+        didSet {
+            layer.shadowRadius = shadowRadius
+        }
+    }
+
+    @IBInspectable var shadowOpacity: CGFloat = 0 {
+        didSet {
+            layer.shadowOpacity = Float(shadowOpacity)
+        }
+    }
+
+    @IBInspectable var shadowOffsetY: CGFloat = 0 {
+        didSet {
+            layer.shadowOffset.height = shadowOffsetY
+        }
+    }
+}
+
---
+++ LoginExample/LoginExample/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  LoginExample
+//
+//  Created by Gary Tokman on 3/10/19.
+//  Copyright © 2019 Gary Tokman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+}
+
