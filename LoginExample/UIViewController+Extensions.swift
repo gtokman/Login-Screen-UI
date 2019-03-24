b50e997f8ea37b4dd4c7b390b0a99d24f8d73c5d
Gary Tokman <hello@garytokman.me>
2019-03-24 14:59:45 -0400

    add sign up

---
+++ LoginExample/UIViewController+Extensions.swift
@@ -0,0 +1,13 @@
+//
+//  UIViewController+Extensions.swift
+//  Login
+//
+//  Created by Gary Tokman on 3/24/19.
+//  Copyright © 2019 Gary Tokman. All rights reserved.
+//
+
+import UIKit
+
+extension UIViewController {
+    @IBAction func unwind(_ segue: UIStoryboardSegue) {}
+}
